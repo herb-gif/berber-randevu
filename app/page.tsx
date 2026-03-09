e44a3ac2fba295f26d0a28b0e67e31bbc31b4425
Hasan Muller <hasan@example.com>
2026-03-09 22:28:33 +0100

    style: simplify booking page wrapper

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
 import BookingWidget from "./components/BookingWidget";
 
 export default function HomePage() {
-  return (
-    <main className="min-h-screen bg-neutral-950">
-      <div className="mx-auto max-w-4xl px-4 py-10">
-        <div className="mt-8">
-          <BookingWidget />
-        </div>
-      </div>
-    </main>
-  );
+  return <BookingWidget />;
 }
